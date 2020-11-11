4af9f51cb768902e258e5146717dc15988161c4d
LIU FRANK <youquan.liu@hotmail.com>
2020-11-11 15:20:59 -0500

    'test'

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -22,6 +22,7 @@ const Product = ({ product }) => {
                 </Card.Text>
                 <Card.Text as='h3'>
                     {product.price}
+                    
                 </Card.Text>
             </Card.Body>
         </Card>
