efa442fd34efb9d655cea76c1793102ff4b3c959
LIU FRANK <youquan.liu@hotmail.com>
2020-11-13 12:25:09 -0500

    1.mongoose models build up; 2.npm colors implmented

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,13 @@
-const express = require('express')
-const products = require('./data/products')
+import express from 'express'
+import dotenv from 'dotenv'
+import colors from 'colors'
+import connectDB from './config/db.js'
+import products from './data/products.js'
+
+dotenv.config()
+
+connectDB()
+
 const app = express()
 
 app.get('/', (req, res) => {
@@ -15,4 +23,10 @@ app.get('/api/products/:id', (req, res) => {
     res.json(product)
 })
 
-app.listen(5000, console.log('server running on port 5000'))
+const PORT = process.env.PORT || 5000
+
+app.listen(
+    PORT,
+    console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`.rainbow.underline.bold)
+)
+
