3a4547d1f9cced785b7c11c5809d1e0519b061cb
LIU FRANK <youquan.liu@hotmail.com>
2020-11-25 21:00:41 -0500

    delete cart item

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col, Image, ListGroup, Card, Button, Form } from 'react-bootstrap'
 import Message from '../components/Message'
-import { addToCart } from '../actions/cartActions'
+import { addToCart, removeFromCart } from '../actions/cartActions'
 
 const CartScreen = ({ match, location, history }) => {
 
@@ -22,9 +22,7 @@ const CartScreen = ({ match, location, history }) => {
     }, [dispatch, productId, qty])
 
     const removeFromCartHandler = (id) => {
-        console.log('remove')
-
-        //dispatch(removeFromCart(id))
+        dispatch(removeFromCart(id))
     }
 
     const checkoutHandler = () => {
