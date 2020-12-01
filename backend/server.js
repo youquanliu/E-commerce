ee94daf336e8a707ea42c450e5131461a99b0456
LIU FRANK <youquan.liu@hotmail.com>
2020-12-01 18:23:35 -0500

    Upload image backend

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import express from 'express'
+import path from 'path'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
+import uploadRoutes from './routes/uploadRoute.js'
 
 import { notFound, errorHandle } from './middleware/errorMiddleware.js'
 
@@ -24,10 +26,15 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
 
 //PayPal Route
 app.get('/api/config/paypal', (req, res) =>
     res.send(process.env.PAYPAL_CLIENT_ID))
+
+//Make uploads folder accessible by setting to static
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 //Custom Error handling middleware 
 app.use(notFound)
