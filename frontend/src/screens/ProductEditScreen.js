3237d85aec2263e529fd80e601c2dc75ead8ec4b
LIU FRANK <youquan.liu@hotmail.com>
2020-12-01 18:35:59 -0500

    set up uploading image function on screen

--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import { Link } from 'react-router-dom'
 import { Form, Button } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
@@ -18,6 +19,7 @@ const ProductEditScreen = ({ match, history }) => {
     const [category, setCategory] = useState('')
     const [countInStock, setCountInStock] = useState(0)
     const [description, setDescription] = useState('')
+    const [uploading, setUploading] = useState(false)
 
     const dispatch = useDispatch()
 
@@ -62,8 +64,25 @@ const ProductEditScreen = ({ match, history }) => {
             })
         )
     }
-    const uploadFileHandler = () => {
+    const uploadFileHandler = async (e) => {
+        const file = e.target.files[0]
+        const formData = new FormData()
+        formData.append('image', file)
+        setUploading(true)
 
+        try {
+            const config = {
+                header: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            }
+            const { data } = await axios.post('/api/upload', formData, config)
+            setImage(data)
+            setUploading(false)
+        } catch (error) {
+            console.error(error)
+            setUploading(false)
+        }
     }
     return (
         <>
@@ -112,7 +131,7 @@ const ProductEditScreen = ({ match, history }) => {
                                     custom
                                     onChange={uploadFileHandler}
                                 ></Form.File>
-                                {/* {uploading && <Loader />} */}
+                                {uploading && <Loader />}
                             </Form.Group>
 
                             <Form.Group controlId='brand'>
