58cfdf847a89567159261ba06e6b720756d86576
LIU FRANK <youquan.liu@hotmail.com>
2020-12-03 16:23:07 -0500

    fixed problom'user name on nav bar stay same after updating user name'

--- frontend/src/reducers/userReducer.js
+++ frontend/src/reducers/userReducer.js
@@ -13,6 +13,7 @@ import {
     USER_UPDATE_PROFILE_REQUEST,
     USER_UPDATE_PROFILE_SUCCESS,
     USER_UPDATE_PROFILE_FAIL,
+    USER_UPDATE_PROFILE_RESET,
     USER_DELETE_REQUEST,
     USER_DELETE_SUCCESS,
     USER_DELETE_FAIL,
@@ -82,6 +83,8 @@ export const userUpdateProfileReducer = (state = { user: {} }, action) => {
             return { loading: false, success: true, user: action.payload }
         case USER_UPDATE_PROFILE_FAIL:
             return { loading: false, error: action.payload }
+        case USER_UPDATE_PROFILE_RESET:
+            return {}
         default:
             return state
     }
