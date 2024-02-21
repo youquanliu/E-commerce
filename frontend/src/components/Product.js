0583bde3e49b6810f9a9a3240060fb83295951ae
youquanliu <youquan.liu@hotmail.com>
2024-02-21 12:46:15 -0500

    display dummy product data on screen

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,31 +1,54 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { Card } from 'react-bootstrap'
-import Rating from './Rating'
+import React from "react";
+import { Link } from "react-router-dom";
+import { Card } from "react-bootstrap";
+import Rating from "./Rating";
 
-const Product = ({ product }) => {
-    return (
-        <Card className='my-3 p-2 rounded'>
-            <Link to={`/product/${product._id}`}>
-                <Card.Img src={product.image} variant='top' />
-            </Link>
+const Product = ({ product_dummy }) => {
+  return (
+    <Card className="my-3 p-3 rounded">
+      {/* -------------------------------- dummy below */}
 
-            <Card.Body>
-                <Link to={`/product/${product._id}`}>
-                    <Card.Title as='div'>
-                        <strong>{product.name}</strong>
-                    </Card.Title>
-                </Link>
+      <a href={`/product/${product_dummy._id}`}>
+        <Card.Img src={product_dummy.image} variant="top" />
+      </a>
+      <Card.Body>
+        <a to={`/product/${product_dummy._id}`}>
+          <Card.Title as="div">
+            <strong>{product_dummy.name}</strong>
+          </Card.Title>
+        </a>
 
-                <Card.Text as='div'>
-                    <Rating value={product.rating} text={`${product.numReviews} reviews`} />
-                </Card.Text>
-                <Card.Text as='h3'>
-                    ${product.price}
-                </Card.Text>
-            </Card.Body>
-        </Card>
-    )
-}
+        <Card.Text as="h3">
+          <Rating
+            value={product_dummy.rating}
+            text={`${product_dummy.numReviews} reviews`}
+          />
+        </Card.Text>
+        <Card.Text as="h3">${product_dummy.price}</Card.Text>
+      </Card.Body>
 
-export default Product
+      {/* -------------------------------- dumm above */}
+      {/* <Link to={`/product/${product._id}`}>
+        <Card.Img src={product.image} variant="top" />
+      </Link>
+
+      <Card.Body>
+        <Link to={`/product/${product._id}`}>
+          <Card.Title as="div">
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </Link>
+
+        <Card.Text as="h3">
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+          />
+        </Card.Text>
+        <Card.Text as="h3">${product.price}</Card.Text>
+      </Card.Body> */}
+    </Card>
+  );
+};
+
+export default Product;
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,67 +1,76 @@
-import React, { useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import { Row, Col } from 'react-bootstrap'
-import { Helmet } from 'react-helmet'
-import Product from '../components/Product'
-import Message from '../components/Message'
-import Loader from '../components/Loader'
-import Paginate from '../components/Paginate'
-import ProductCarousel from '../components/ProductCarousel'
-import { listProducts } from '../actions/productActions'
-import Meta from '../components/Meta'
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { Row, Col } from "react-bootstrap";
+import Product from "../components/Product";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
+import Paginate from "../components/Paginate";
+import ProductCarousel from "../components/ProductCarousel";
+import { listProducts } from "../actions/productActions";
+import Meta from "../components/Meta";
+
+//Dummy data for display
+import Product_dummy from "../../src/product.js";
 
 const HomeScreen = ({ match }) => {
+  const keyword = match.params.keyword;
+  const pageNumber = match.params.pageNumber || 1;
 
-    const keyword = match.params.keyword
-    const pageNumber = match.params.pageNumber || 1
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch()
+  const productList = useSelector((state) => state.productList);
 
-    const productList = useSelector(state => state.productList)
+  const { loading, error, products, page, pages } = productList;
 
-    const { loading, error, products, page, pages } = productList
+  useEffect(() => {
+    dispatch(listProducts(keyword, pageNumber));
+  }, [dispatch, keyword, pageNumber]);
 
-    useEffect(() => {
-        dispatch(listProducts(keyword, pageNumber))
-    }, [dispatch, keyword, pageNumber])
-
-    return (
+  return (
+    <>
+      <Meta />
+      {!keyword ? (
+        <ProductCarousel />
+      ) : (
+        <Link to="/" className="btn btn-info">
+          Go Back
+        </Link>
+      )}
+      <h1>latest Products</h1>
+      {loading ? (
+        <Loader></Loader>
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
         <>
-            <Meta />
-            {!keyword ? (
-                <ProductCarousel />
-            ) : (
-                    <Link to='/' className='btn btn-info'>
-                        Go Back
-                    </Link>
-                )}
-            <h1>latest Products</h1>
-            {loading
-                ? (<Loader></Loader>)
-                : error
-                    ? (<Message variant='danger'>{error}</Message>)
-                    : (<>
-                        <Row>
-                            {console.log('%c Products List:', 'color:orange; font-wight:bold; font-size:20px')}
-                            {console.table(products)}
-                            {products.map((product) => (
-                                <Col key={product._id} sm={12} md={6} lg={4} xl={3} >
-                                    <Product product={product} />
-                                </Col>
-                            ))}
-                        </Row>
-                        <Paginate
-                            pages={pages}
-                            page={page}
-                            keyword={keyword ? keyword : ''}
-                        />
-                    </>
-                    )
-            }
-
+          <Row>
+            {console.log(
+              "%c Products List:",
+              "color:orange; font-wight:bold; font-size:20px"
+            )}
+            {console.table(products)}
+            {products.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            ))}
+            {/* Dummy below ----------------------- */}
+            {Product_dummy.map((dummy_product) => (
+              <Col key={dummy_product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product_dummy={dummy_product} />
+              </Col>
+            ))}
+          </Row>
+          <Paginate
+            pages={pages}
+            page={page}
+            keyword={keyword ? keyword : ""}
+          />
         </>
-    )
-}
+      )}
+    </>
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
