b39c9ef05141828052cb48860918f11d60fd9bd7
youquanliu <youquan.liu@hotmail.com>
2024-02-23 18:31:44 -0500

    import Helmet from react-helmet-async instead

--- frontend/src/components/Meta.js
+++ frontend/src/components/Meta.js
@@ -1,20 +1,20 @@
-import React from 'react'
-import { Helmet } from 'react-helmet'
+import React from "react";
+import { Helmet } from "react-helmet-async";
 
 const Meta = ({ title, description, keywords }) => {
-    return (
-        <Helmet>
-            <title>{title}</title>
-            <meta name='description' content={description} />
-            <meta name='keyword' content={keywords} />
-        </Helmet>
-    )
-}
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta name="keyword" content={keywords} />
+    </Helmet>
+  );
+};
 
 Meta.defaultProps = {
-    title: "Welcome to Frank's Toilet Paper Depot",
-    description: 'We sell the best products for cheap',
-    keywords: 'toliet paper, creative toliet paper, toliet',
-}
+  title: "Welcome to Frank's Toilet Paper Depot",
+  description: "We sell the best products for cheap",
+  keywords: "toliet paper, creative toliet paper, toliet",
+};
 
-export default Meta
+export default Meta;
