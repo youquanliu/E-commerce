b9b263ea0322badf1906b4f6dbb96a155cf84bc8
youquanliu <youquan.liu@hotmail.com>
2024-03-01 18:05:26 -0500

    Use Redux slice for state management instead of UseEffect

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,10 +1,8 @@
-import { useEffect, useState } from "react";
 import { Row, Col } from "react-bootstrap";
 import { useParams } from "react-router-dom";
 import { useGetProductsQuery } from "../slices/productsApiSlice";
 import { Link } from "react-router-dom";
 import Product from "../components/Product";
-import axios from "axios";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
 import Paginate from "../components/Paginate";
@@ -13,33 +11,36 @@ import Meta from "../components/Meta"; //Dummy data for display
 //Dummy data for display
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([]);
   const { pageNumber, keyword } = useParams();
 
-  const { data, isLoading, error } = useGetProductsQuery({
+  const {
+    data: products,
+    isLoading,
+    error,
+  } = useGetProductsQuery({
     keyword,
     pageNumber,
   });
 
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get(`/api/products`);
-      setProducts(data);
-    };
-    fetchProducts();
-  }, []);
-
   return (
     <>
-      <h1>Latest Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <h3>{product.name}</h3>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {isLoading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error?.data?.message || error.error}</Message>
+      ) : (
+        <>
+          <h1>Latest Products</h1>
+          <Row>
+            {products.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <h3>{product.name}</h3>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+        </>
+      )}
     </>
   );
 };
