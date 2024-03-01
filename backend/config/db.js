c572ea49da45a286f1227bc4b5ec43e0853b9675
youquanliu <youquan.liu@hotmail.com>
2024-02-29 21:01:50 -0500

    slight change in dbjs

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
+import colors from "colors";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.DATABASE_URL, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-    });
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    const conn = await mongoose.connect(process.env.DATABASE_URL);
+    console.log(
+      `MongoDB Connected: ${conn.connection.host}`.green.underline.bold
+    );
   } catch (error) {
     console.error(`Error: ${error.message}`.red.underline.bold);
     //exit with faliure
