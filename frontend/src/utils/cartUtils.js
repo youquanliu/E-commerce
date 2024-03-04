16ee0533b224bc1d32b8f324203de4e8a6d5901e
youquanliu <youquan.liu@hotmail.com>
2024-03-04 13:17:31 -0500

    Helper function to calculate total peice in cart

--- frontend/src/utils/cartUtils.js
+++ frontend/src/utils/cartUtils.js
@@ -2,11 +2,6 @@ export const addDecimals = (num) => {
     return (Math.round(num * 100) / 100).toFixed(2);
   };
   
-  // NOTE: the code below has been changed from the course code to fix an issue
-  // with type coercion of strings to numbers.
-  // Our addDecimals function expects a number and returns a string, so it is not
-  // correct to call it passing a string as the argument.
-  
   export const updateCart = (state) => {
     // Calculate the items price in whole number (pennies) to avoid issues with
     // floating point number calculations
@@ -21,7 +16,7 @@ export const addDecimals = (num) => {
     state.shippingPrice = addDecimals(shippingPrice);
   
     // Calculate the tax price
-    const taxPrice = 0.15 * itemsPrice;
+    const taxPrice = 0.13 * itemsPrice;
     state.taxPrice = addDecimals(taxPrice);
   
     const totalPrice = itemsPrice + shippingPrice + taxPrice;
