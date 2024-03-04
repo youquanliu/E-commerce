f9af1866a0d96a4076713ed075f21898ac494489
youquanliu <youquan.liu@hotmail.com>
2024-03-04 13:16:48 -0500

    Cart slice for state mangement

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -1,19 +1,26 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { updateCart } from '../utils/cartUtils';
+import { createSlice } from "@reduxjs/toolkit";
+import { updateCart } from "../utils/cartUtils";
 
-const initialState = localStorage.getItem('cart')
-  ? JSON.parse(localStorage.getItem('cart'))
-  : { cartItems: [], shippingAddress: {}, paymentMethod: 'PayPal' };
+//Store items in the local storage as initial state
+const initialState = localStorage.getItem("cart")
+  ? JSON.parse(localStorage.getItem("cart"))
+  : { cartItems: [], shippingAddress: {}, paymentMethod: "PayPal" };
 
+// The reason using createSlice instead of apiSlice like product,order and user is:
+// Cart is not the endpoints dealing with asynchronous request
 const cartSlice = createSlice({
-  name: 'cart',
+  name: "cart",
   initialState,
+  //reducers object contains mutiple functions to do with the cart like:
+  //addToCart, removeFromCart
   reducers: {
+    //state: current state in cart
+    //action: data inside of payload
     addToCart: (state, action) => {
-      // NOTE: we don't need user, rating, numReviews or reviews
-      // in the cart
       const { user, rating, numReviews, reviews, ...item } = action.payload;
-
+      console.log("this is from cartSlice.js");
+      console.log("action.payload: ", action.payload);
+      console.log("state: ", state);
       const existItem = state.cartItems.find((x) => x._id === item._id);
 
       if (existItem) {
@@ -23,25 +30,29 @@ const cartSlice = createSlice({
       } else {
         state.cartItems = [...state.cartItems, item];
       }
-
       return updateCart(state, item);
     },
+
     removeFromCart: (state, action) => {
       state.cartItems = state.cartItems.filter((x) => x._id !== action.payload);
       return updateCart(state);
     },
+
     saveShippingAddress: (state, action) => {
       state.shippingAddress = action.payload;
-      localStorage.setItem('cart', JSON.stringify(state));
+      localStorage.setItem("cart", JSON.stringify(state));
     },
+
     savePaymentMethod: (state, action) => {
       state.paymentMethod = action.payload;
-      localStorage.setItem('cart', JSON.stringify(state));
+      localStorage.setItem("cart", JSON.stringify(state));
     },
+
     clearCartItems: (state, action) => {
       state.cartItems = [];
-      localStorage.setItem('cart', JSON.stringify(state));
+      localStorage.setItem("cart", JSON.stringify(state));
     },
+
     // NOTE: here we need to reset state for when a user logs out so the next
     // user doesn't inherit the previous users cart and shipping
     resetCart: (state) => (state = initialState),
