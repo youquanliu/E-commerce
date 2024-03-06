12da35aeaf79e16cf29537e1d703ad4eb77c813e
youquanliu <youquan.liu@hotmail.com>
2024-03-06 15:51:08 -0500

    Protect and Admin middleware to check authication

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,41 +1,38 @@
-import jwt from 'jsonwebtoken'
-import User from '../models/userModel.js'
-import asyncHandler from '../middleware/asyncHandler.js';
+import jwt from "jsonwebtoken";
+import User from "../models/userModel.js";
+import asyncHandler from "./asyncHandler.js";
 
 const protect = asyncHandler(async (req, res, next) => {
-    let token
-    if (
-        req.headers.authorization
-        && req.headers.authorization.startsWith('Bearer')
-    ) {
-        try {
-            //req.headers.authorization come with 'Bearer ****token***'
-            token = req.headers.authorization.split(' ')[1]
-            const decoded = jwt.verify(token, process.env.JWT_SECRET)
+  let token;
 
-            //select('-'): exclude password, include other fields
-            req.user = await User.findById(decoded.id).select('-password')
-            next()
-        } catch (error) {
-            console.error(error)
-            res.status(401)
-            throw new Error('Not authorized, token failed')
-        }
-    }
+  // Read JWT from the 'jwt' cookie
+  token = req.cookies.jwt;
 
-    if (!token) {
-        res.status(401)
-        throw new Error('Not Authorized, no token')
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      //select('-'): exclude password, include other fields
+      req.user = await User.findById(decoded.userId).select("-password");
+      next();
+    } catch (error) {
+      console.error(error);
+      res.status(401);
+      throw new Error("Not authorized, token failed");
     }
-})
+  } else {
+    res.status(401);
+    throw new Error("Not Authorized, no token");
+  }
+});
 
 const admin = (req, res, next) => {
-    if (req.user && req.user.isAdmin) {
-        next()
-    } else {
-        res.status(401)
-        throw new Error('Not authorized as an admin')
-    }
-}
+  if (req.user && req.user.isAdmin) {
+    next();
+  } else {
+    res.status(401);
+    throw new Error("Not authorized as an admin");
+  }
+};
 
-export { protect, admin }
+export { protect, admin };
