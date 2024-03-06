37f8795ea7bbefc22b8b8aa8e8e8849ef1113da1
youquanliu <youquan.liu@hotmail.com>
2024-03-06 16:16:08 -0500

    User update profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -12,7 +12,7 @@ const authUser = asyncHandler(async (req, res) => {
   //password is in plain text, we need to bcrypt in user Model
   if (user && (await user.matchPassword(password))) {
     generateToken(res, user._id);
-    res.json({
+    res.status(200).json({
       _id: user._id,
       name: user.name,
       email: user.email,
@@ -68,19 +68,19 @@ const logoutUser = asyncHandler(async (req, res) => {
 //@Route GET /api/users/profile
 //@access Private
 const getUserProfile = asyncHandler(async (req, res) => {
-  //   const user = await User.findById(req.user._id);
+  const user = await User.findById(req.user._id);
 
-  //   if (user) {
-  //     res.json({
-  //       _id: user._id,
-  //       name: user.name,
-  //       email: user.email,
-  //       isAdmin: user.isAdmin,
-  //     });
-  //   } else {
-  //     res.status(404);
-  //     throw new Error("user not found!");
-  //   }
+  if (user) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("user not found!");
+  }
 
   res.send("Profile!");
 });
@@ -99,12 +99,11 @@ const updateUserProfile = asyncHandler(async (req, res) => {
     }
 
     const updatedUser = await user.save();
-    res.json({
+    res.status(200).json({
       _id: updatedUser._id,
       name: updatedUser.name,
       email: updatedUser.email,
       isAdmin: updatedUser.isAdmin,
-      token: generateToken(updatedUser._id),
     });
   } else {
     res.status(404);
