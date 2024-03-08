8676a1f2811af9360acefb4f22fff83f25ee96a5
youquanliu <youquan.liu@hotmail.com>
2024-03-08 18:28:56 -0500

    Shipping screen

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -1,85 +1,87 @@
-import React, { useState, useEffect } from 'react'
-import { Form, Button } from 'react-bootstrap'
-import { useDispatch, useSelector } from 'react-redux'
-import FormContainer from '../components/FormContainer'
-import CheckoutSteps from '../components/CheckoutSteps'
-import { saveShippingAddress } from '../actions/cartActions'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Form, Button } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import FormContainer from "../components/FormContainer";
+import CheckoutSteps from "../components/CheckoutSteps";
+import { saveShippingAddress } from "../slices/cartSlice";
 
-const ShippingScreen = ({ history }) => {
+const ShippingScreen = () => {
+  const cart = useSelector((state) => state.cart);
+  const { shippingAddress } = cart;
 
-    const cart = useSelector(state => state.cart)
-    const { shippingAddress } = cart
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    const [address, setAddress] = useState(shippingAddress.address)
-    const [city, setCity] = useState(shippingAddress.city)
-    const [postalCode, setPostalCode] = useState(shippingAddress.postalCode)
-    const [country, setCountry] = useState(shippingAddress.country)
+  const [address, setAddress] = useState(shippingAddress.address || "");
+  const [city, setCity] = useState(shippingAddress.city || "");
+  const [postalCode, setPostalCode] = useState(
+    shippingAddress.postalCode || ""
+  );
+  const [country, setCountry] = useState(shippingAddress.country || "");
 
-    const dispatch = useDispatch()
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(saveShippingAddress({ address, city, postalCode, country }));
+    navigate("/payment");
+  };
+  return (
+    <FormContainer>
+      {/* //------------------Checkout steps component */}
+      <CheckoutSteps step1 step2 />
 
-    const submitHandler = (e) => {
-        e.preventDefault()
-        dispatch(saveShippingAddress({ address, city, postalCode, country }))
-        history.push('/payment')
-    }
-    return (
-        <FormContainer>
-            {/* //------------------Checkout steps component */}
-            <CheckoutSteps step1 step2/>
-            
-            <h1>Shipping</h1>
-            <Form onSubmit={submitHandler}>
+      <h1>Shipping</h1>
+      <Form onSubmit={submitHandler}>
+        <Form.Group controlId="address" className="my-3">
+          <Form.Label>Address</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter Address"
+            value={address}
+            required
+            onChange={(e) => setAddress(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
 
-                <Form.Group controlId='address'>
-                    <Form.Label>Address</Form.Label>
-                    <Form.Control
-                        type='text'
-                        placeholder='Enter Address'
-                        value={address}
-                        required
-                        onChange={(e) => setAddress(e.target.value)}>
-                    </Form.Control>
-                </Form.Group>
+        <Form.Group controlId="city" className="my-3">
+          <Form.Label>City</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter City"
+            value={city}
+            required
+            onChange={(e) => setCity(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
 
-                <Form.Group controlId='city'>
-                    <Form.Label>City</Form.Label>
-                    <Form.Control
-                        type='text'
-                        placeholder='Enter City'
-                        value={city}
-                        required
-                        onChange={(e) => setCity(e.target.value)}>
-                    </Form.Control>
-                </Form.Group>
+        <Form.Group controlId="postalCode" className="my-3">
+          <Form.Label>PostalCode</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter PostalCode"
+            value={postalCode}
+            required
+            onChange={(e) => setPostalCode(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
 
-                <Form.Group controlId='postalCode'>
-                    <Form.Label>PostalCode</Form.Label>
-                    <Form.Control
-                        type='text'
-                        placeholder='Enter PostalCode'
-                        value={postalCode}
-                        required
-                        onChange={(e) => setPostalCode(e.target.value)}>
-                    </Form.Control>
-                </Form.Group>
+        <Form.Group controlId="country" className="my-3">
+          <Form.Label>Country</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter Country"
+            value={country}
+            required
+            onChange={(e) => setCountry(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
 
-                <Form.Group controlId='country'>
-                    <Form.Label>Country</Form.Label>
-                    <Form.Control
-                        type='text'
-                        placeholder='Enter Country'
-                        value={country}
-                        required
-                        onChange={(e) => setCountry(e.target.value)}>
-                    </Form.Control>
-                </Form.Group>
+        <Button type="submit" variant="success" className="my-3">
+          Continue
+        </Button>
+      </Form>
+    </FormContainer>
+  );
+};
 
-                <Button type='submit' variant='success'>
-                    Continue
-            </Button>
-            </Form>
-        </FormContainer>
-    )
-}
-
-export default ShippingScreen
+export default ShippingScreen;
