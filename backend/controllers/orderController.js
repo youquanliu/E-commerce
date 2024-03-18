364de5fe900b8af7db25cbb4b109ea62b5cc4386
youquanliu <youquan.liu@hotmail.com>
2024-03-18 17:44:17 -0400

    Order Controller

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -98,20 +98,19 @@ const getOrders = asyncHandler(async (req, res) => {
 // @route   PUT /api/orders/:id/deliver
 // @access  Private/Admin
 const updateOrderToDelivered = asyncHandler(async (req, res) => {
-  //   const order = await Order.findById(req.params.id);
+  const order = await Order.findById(req.params.id);
 
-  //   if (order) {
-  //     order.isDelivered = true;
-  //     order.deliveredAt = Date.now();
+  if (order) {
+    order.isDelivered = true;
+    order.deliveredAt = Date.now();
 
-  //     const updatedOrder = await order.save();
+    const updatedOrder = await order.save();
 
-  //     res.json(updatedOrder);
-  //   } else {
-  //     res.status(404);
-  //     throw new Error("Order not found");
-  //   }
-  res.send("updateOrderToDelivered");
+    res.json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
 });
 export {
   addOrderItems,
