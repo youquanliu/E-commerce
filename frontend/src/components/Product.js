250a5e497def55dbbd3177ce0e26c84d2b32512a
youquanliu <youquan.liu@hotmail.com>
2024-03-18 19:29:34 -0400

    Fix two price display on homw screen

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,52 +1,30 @@
-import React from "react";
-import { Link } from "react-router-dom";
 import { Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import Rating from "./Rating";
 
 const Product = ({ product }) => {
   return (
     <Card className="my-3 p-3 rounded">
-      {/* -------------------------------- dummy below */}
-
       <Link to={`/product/${product._id}`}>
         <Card.Img src={product.image} variant="top" />
       </Link>
+
       <Card.Body>
         <Link to={`/product/${product._id}`}>
           <Card.Title as="div" className="product-title">
             <strong>{product.name}</strong>
           </Card.Title>
         </Link>
-        <Card.Text as="h3">${product.price}</Card.Text>
+
         <Card.Text as="div">
           <Rating
             value={product.rating}
             text={`${product.numReviews} reviews`}
           />
         </Card.Text>
+
         <Card.Text as="h3">${product.price}</Card.Text>
       </Card.Body>
-
-      {/* -------------------------------- dumm above */}
-      {/* <Link to={`/product/${product._id}`}>
-        <Card.Img src={product.image} variant="top" />
-      </Link>
-
-      <Card.Body>
-        <Link to={`/product/${product._id}`}>
-          <Card.Title as="div">
-            <strong>{product.name}</strong>
-          </Card.Title>
-        </Link>
-
-        <Card.Text as="h3">
-          <Rating
-            value={product.rating}
-            text={`${product.numReviews} reviews`}
-          />
-        </Card.Text>
-        <Card.Text as="h3">${product.price}</Card.Text>
-      </Card.Body> */}
     </Card>
   );
 };
