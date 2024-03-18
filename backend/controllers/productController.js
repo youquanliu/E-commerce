83e1312909d0d12b4d31cc2d1502664797f3855d
youquanliu <youquan.liu@hotmail.com>
2024-03-18 19:41:24 -0400

    set defualt sample image when creating a new product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -74,7 +74,7 @@ const createProduct = asyncHandler(async (req, res) => {
     name: "Sample name",
     price: 0,
     user: req.user._id,
-    image: "/images/sample.png",
+    image: "/images/sample.jpg",
     brand: "Sample brand",
     category: "Sample category",
     countInStock: 0,
