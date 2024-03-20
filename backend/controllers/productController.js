7ccdabdf9e211ff2381268d00e18129cc1088f70
youquanliu <youquan.liu@hotmail.com>
2024-03-20 14:35:10 -0400

    Review and commit at Product controller and route

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -119,6 +119,7 @@ const createProductReview = asyncHandler(async (req, res) => {
   const product = await Product.findById(req.params.id);
 
   if (product) {
+    
     const alreadyReviewed = product.reviews.find(
       (r) => r.user.toString() === req.user._id.toString()
     );
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,22 +1,22 @@
 import express from "express";
 const router = express.Router();
-import colors from "colors";
 import {
   getProducts,
   getProductById,
   createProduct,
   updateProduct,
   deleteProduct,
+  createProductReview,
 } from "../controllers/productController.js";
 //
-//   createProductReview,
+//
 //   getTopProduct,
 
 import { protect, admin } from "../middleware/authMiddleware.js";
 
 router.route("/").get(getProducts).post(protect, admin, createProduct);
 
-// router.route("/:id/reviews").post(protect, createProductReview);
+router.route("/:id/reviews").post(protect, createProductReview);
 
 // router.get("/top", getTopProduct);
 
