340da59fbf83d853c6d8cf017b88edab858633d4
youquanliu <youquan.liu@hotmail.com>
2024-04-01 12:03:37 -0400

    Usr Mongoose isValidObjectId to replace my custom objectID checking method

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -10,11 +10,6 @@ const errorHandle = (err, req, res, next) => {
   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
   let message = err.message;
 
-  if (err.name === "CastError" && err.kind === "ObjectId") {
-    message = `Resource not found`;
-    statusCode = 404;
-  }
-
   res.status(statusCode).json({
     message,
     stack: process.env.NODE_ENV === "production" ? "🍔" : err.stack,
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -11,17 +11,18 @@ import {
 } from "../controllers/productController.js";
 
 import { protect, admin } from "../middleware/authMiddleware.js";
+import checkObjectId from "../middleware/checkObjectId.js";
 
 router.route("/").get(getProducts).post(protect, admin, createProduct);
 
-router.route("/:id/reviews").post(protect, createProductReview);
+router.route("/:id/reviews").post(protect, checkObjectId, createProductReview);
 
 router.get("/top", getTopProduct);
 
 router
   .route("/:id")
-  .get(getProductById)
-  .put(protect, admin, updateProduct)
-  .delete(protect, admin, deleteProduct);
+  .get(checkObjectId, getProductById)
+  .put(protect, admin, checkObjectId, updateProduct)
+  .delete(protect, admin, checkObjectId, deleteProduct);
 
 export default router;
