570428219cfc7031efdd92895f733d3e6a1946ba
youquanliu <youquan.liu@hotmail.com>
2024-04-01 11:40:37 -0400

    prepare for Production: 1. server.js if NODE_ENV is production, server load build version of React 2. build script in package.json 3. Ran build sctipt, and generate build folder

--- backend/server.js
+++ backend/server.js
@@ -25,8 +25,6 @@ app.use(express.urlencoded({ extended: true }));
 // Cookie parser middleware
 app.use(cookieParser());
 
-app.get("/", (req, res) => res.send("API is running"));
-
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
